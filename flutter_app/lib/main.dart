c9eacc7c9c9955856b846bb289c05840bbb5b6db
chuong1210 <chuongbd1012@gmail.com>
2025-04-04 20:44:44 +0700

    login

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -36,7 +36,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Ride Hailing App',
+      title: 'Ride OS App',
       debugShowCheckedModeBanner: false,
       theme: AppTheme.lightTheme,
       home: const SplashScreen(),
